4ef05ff6eed2c2eea4afa01a7b9800bf6074fe0b
Unknown <filip.prvulj@gmail.com>
2020-05-11 15:32:39 +0200

    Add: Post i Get request za grupu zadataka

---
+++ BrainfinityAPI/Controllers/GrupaZadatakaController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BrainfinityAPI.Services;
+using BrainfinityAPI.Dtos;
+
+namespace BrainfinityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrupaZadatakaController : ControllerBase
+    {
+        private readonly IGrupaZadatakaService _gzs;
+
+        public GrupaZadatakaController(IGrupaZadatakaService gzs)
+        {
+            _gzs = gzs;
+        }
+
+        [HttpGet("{takmicenjeId}")]
+        public IActionResult GetSveGrupeZadataka(int takmicenjeId)
+        {
+            return Ok(_gzs.GetSveGrupeZadataka(takmicenjeId));
+        }
+
+        [HttpGet]
+        [Route("Grupa/{id}")]
+        public IActionResult GetGrupaZadataka(int id)
+        {
+            var grupaZadataka = _gzs.GetGrupaZadataka(id);
+            if (grupaZadataka == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(grupaZadataka);
+        }
+
+        [HttpPost]
+        public IActionResult NovaGrupaZadataka([FromBody]UpdateGrupaZadatakaDto grupaZadataka)
+        {
+            if (_gzs.NovaGrupaZadataka(grupaZadataka))
+            {
+                return Ok(grupaZadataka);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}

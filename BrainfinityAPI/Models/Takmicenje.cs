c89fce08d9704ddeaa19dc5c6d13ffcf9d9bf194
Unknown <filip.prvulj@gmail.com>
2020-06-02 16:11:07 +0200

    Add: Status u takmicenje modelu

--- BrainfinityAPI/Models/Takmicenje.cs
+++ BrainfinityAPI/Models/Takmicenje.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BrainfinityAPI.Models
 {
-    public enum Status
-    {
-        Arhivirano,
-        Aktivno,
-        Nastupajuce
-    }
+    //public enum Status
+    //{
+    //    Arhivirano,
+    //    Aktivno,
+    //    Nastupajuce
+    //}
 
     public class Takmicenje
     {
@@ -30,7 +31,8 @@ namespace BrainfinityAPI.Models
 
         public string Slika { get; set; }
 
-        [Required]
-        public Status Status { get; set; }
+        public virtual Status Status { get; set; }
+
+        public int StatusId { get; set; }
     }
 }

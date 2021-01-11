30a85b54936d8079384bfdbc026b9edec5992b3c
F <filip.prvulj@gmail.com>
2021-01-11 03:25:38 +0100

    Add: Tabela Korisnik i popunjavanje tabele AspNetRoles

---
+++ BrainfinityAPI/Data/Migrations/20200616204415_DodajTabeluKorisnik.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrainfinityAPI.Data.Migrations
+{
+    public partial class DodajTabeluKorisnik : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ime",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Logo",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Prezime",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Slika",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ime",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Logo",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Prezime",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Slika",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ BrainfinityAPI/Data/Migrations/20200616221445_PopunjavanjeAspNetRolesTabele.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrainfinityAPI.Data.Migrations
+{
+    public partial class PopunjavanjeAspNetRolesTabele : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO aspNetRoles (Id, Name, NormalizedName) VALUES (1,'Supervizor', 'SUPERVIZOR')");
+            migrationBuilder.Sql("INSERT INTO aspNetRoles (Id, Name, NormalizedName) VALUES (2,'Pregledac', 'PREGLEDAC')");
+            migrationBuilder.Sql("INSERT INTO aspNetRoles (Id, Name, NormalizedName) VALUES (3,'Glavni pregledac', 'GLAVNIPREGLEDAC')");
+            migrationBuilder.Sql("INSERT INTO aspNetRoles (Id, Name, NormalizedName) VALUES (4,'Tim', 'TIM')");
+            migrationBuilder.Sql("INSERT INTO aspNetRoles (Id, Name, NormalizedName) VALUES (5,'Dizajner', 'DIZAJNER')");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}

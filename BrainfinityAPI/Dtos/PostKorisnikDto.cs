604239242dc61392d65a4ab7927d370ab80b695c
F <filip.prvulj@gmail.com>
2021-01-11 03:19:56 +0100

    Add: PostKorisnikDto

---
+++ BrainfinityAPI/Dtos/PostKorisnikDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainfinityAPI.Dtos
+{
+    public class PostKorisnikDto
+    {
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public byte[] Slika { get; set; }
+        public byte[] Logo { get; set; }
+        public string KorisnickiTip { get; set; }
+    }
+}

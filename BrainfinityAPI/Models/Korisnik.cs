47bfbe82d9e2a6f55ba05e366f41cb344276c8a1
F <filip.prvulj@gmail.com>
2021-01-11 03:14:51 +0100

    Add: Korisnik model

---
+++ BrainfinityAPI/Models/Korisnik.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainfinityAPI.Models
+{
+    public class Korisnik : IdentityUser
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public byte[] Slika { get; set; }
+        public byte[] Logo { get; set; }
+    }
+}
